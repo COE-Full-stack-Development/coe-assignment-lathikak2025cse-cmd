13a6ae0b52cdb7185720d4828298f6dcd6e7cb77
lathikak2025cse-cmd <lathika.k2025cse@sece.ac.in>
2026-07-22 15:32:02 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-(() => {
-  
-// your code goe here
 
-  });
-})();
+document.getElementById("toggle-theme-btn").onclick = function () {
+    document.getElementById("profile-card").classList.toggle("dark-theme");
+};
+
+
+document.getElementById("add-skill-btn").onclick = function () {
+
+    let skill = document.getElementById("skill-input").value;
+
+    if (skill.trim() != "") {
+
+        let newSkill = document.createElement("li");
+
+        newSkill.innerHTML = skill;
+
+        document.getElementById("skills-list").appendChild(newSkill);
+
+        document.getElementById("skill-input").value = "";
+    }
+
+};
